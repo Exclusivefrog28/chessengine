2830cc0789cc2214ee3163f8797cb963c2a53861
excf28 <deee4444@gmail.com>
2023-04-17 10:43:31 +0200

    perft function exported

--- main.cpp
+++ main.cpp
@@ -120,12 +120,20 @@ void setFen(char *fen) {
 }
 
 EMSCRIPTEN_KEEPALIVE
+int runPerft(int depth, const char* fen) {
+    ChessBoard perftBoard;
+    perftBoard.setPosition(fen);
+
+    return MoveGenerator::perft(depth, perftBoard);
+}
+
+EMSCRIPTEN_KEEPALIVE
 int main() {
 //    board.setStartingPosition();
 //
 //    std::cout << board.fen() << std::endl;
 //
-//    board.setPosition("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
+//    board.setPosition("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
 //
 //    std::cout << board.fen() << std::endl;
 //
