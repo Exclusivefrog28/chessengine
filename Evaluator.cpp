6902fa39fe4c69f924cb060a2a686431b365e311
excf28 <deee4444@gmail.com>
2023-04-29 14:31:28 +0200

    piece-square table eval

--- Evaluator.cpp
+++ Evaluator.cpp
@@ -2,23 +2,49 @@
 
 int Evaluator::evaluate(const ChessBoard &board) {
 
-    int score = 0;
+    int gamePhase = 0;
+    int whiteTableMidScore = 0;
+    int whiteTableEndScore = 0;
+    int blackTableMidScore = 0;
+    int blackTableEndScore = 0;
 
-    score += materialScore(board, board.sideToMove);
-    score -= materialScore(board, Util::invertColor(board.sideToMove));
+    for (const Piece &piece: board.whitePieces) {
+        short index = piece.type - 1;
+        whiteTableMidScore += mg_value[index] + mg_pesto_table[index][piece.position];
+        whiteTableEndScore += eg_value[index] + eg_pesto_table[index][piece.position];
+        gamePhase += gamephaseInc[index];
+    }
+    for (const short &pos: board.whitePawns) {
+        whiteTableMidScore += mg_value[0] + mg_pesto_table[0][pos];
+        whiteTableEndScore += eg_value[0] + eg_pesto_table[0][pos];
+    }
+    for (const Piece &piece: board.blackPieces) {
+        short index = piece.type - 1;
+        short position = flipPosition(piece.position);
+        blackTableMidScore += mg_value[index] + mg_pesto_table[index][position];
+        blackTableEndScore += eg_value[index] + eg_pesto_table[index][position];
+        gamePhase += gamephaseInc[index];
+    }
+    for (const short &pos: board.blackPawns) {
+        short position = flipPosition(pos);
+        blackTableMidScore += mg_value[0] + mg_pesto_table[0][position];
+        blackTableEndScore += eg_value[0] + eg_pesto_table[0][position];
+    }
 
-    return score;
+    int midGameScore = (board.sideToMove == WHITE) ? whiteTableMidScore - blackTableMidScore : blackTableMidScore -
+                                                                                               whiteTableMidScore;
+    int endGameScore = (board.sideToMove == WHITE) ? whiteTableEndScore - blackTableEndScore : blackTableEndScore -
+                                                                                               whiteTableEndScore;
+
+    int midGamePhase = (gamePhase > 24) ? 24 : gamePhase;
+    int endGamePhase = 24 - midGamePhase;
+
+    return (midGameScore * midGamePhase + endGameScore * endGamePhase) / 24;
 }
 
-int Evaluator::materialScore(const ChessBoard &board, Color side) {
-    const std::vector<Piece> *pieces = (side == WHITE) ? &board.whitePieces : &board.blackPieces;
-    const std::vector<short> *pawns = (side == WHITE) ? &board.whitePawns : &board.blackPawns;
+short Evaluator::flipPosition(short pos) {
+    short file = pos % 8;
+    short rank = pos / 8;
 
-    int score = 0;
-    for (Piece piece : *pieces) {
-        score += materialValues[piece.type - 1];
-    }
-    score += materialValues[0] * pawns->size();
-
-    return score;
+    return (7 - rank) * 8 + file;
 }
--- Evaluator.h
+++ Evaluator.h
@@ -2,26 +2,16 @@
 #define CHESSENGINE_EVALUATOR_H
 
 #include "ChessBoard.h"
+#include "EvaluationValues.h"
+
+using namespace EvaluationValues;
 
 class Evaluator {
-
-    enum MaterialValues {
-        PAWN_VALUE = 100,
-        KNIGHT_VALUE = 350,
-        BISHOP_VALUE = 350,
-        ROOK_VALUE = 525,
-        QUEEN_VALUE = 1000,
-    };
-
 
 public:
     static int evaluate(const ChessBoard &board);
 
 private:
-
-    constexpr static std::array<int,5> materialValues = {100, 350, 350, 525, 1000};
-
-    static int materialScore(const ChessBoard &board, Color side);
+    static short flipPosition(short pos);
 };
-
 #endif //CHESSENGINE_EVALUATOR_H
